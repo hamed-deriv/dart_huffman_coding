5936d379b5dbc8d56baa161898b2fada2ee88ae3
hamed-fs <hamed@firstsource.io>
2021-08-03 13:43:00 +0430

    implement huffman coding algorith

--- lib/dart_huffman_coding.dart
+++ lib/dart_huffman_coding.dart
@@ -1,3 +1,92 @@
-int calculate() {
-  return 6 * 7;
+void main(List<String> arguments) {
+  final Map<String, int> result = sortCharacters(
+    getCharacterCount(
+      'In computer science and information theory, a Huffman code is a particular type of optimal prefix code that is commonly used for lossless data compression.',
+    ),
+  );
+
+  printTree(generateTree(result));
+}
+
+void printTree(NodeTree? node) {
+  print('${node!.char} ${node.value}');
+
+  if (node.left != null) {
+    printTree(node.left);
+  }
+
+  if (node.right != null) {
+    printTree(node.right);
+  }
+}
+
+Map<String, int> getCharacterCount(String input) {
+  final Map<String, int> result = <String, int>{};
+
+  for (int i = 0; i < input.length; i++) {
+    result.containsKey(input[i])
+        ? result[input[i]] = result[input[i]]! + 1
+        : result[input[i]] = 1;
+  }
+
+  return result;
+}
+
+Map<String, int> sortCharacters(Map<String, int> characters) {
+  final List<MapEntry<String, int>> mapEntries = characters.entries.toList()
+    ..sort(
+      (MapEntry<String, int> first, MapEntry<String, int> second) =>
+          first.value.compareTo(second.value),
+    );
+
+  characters
+    ..clear()
+    ..addEntries(mapEntries);
+
+  return characters;
+}
+
+NodeTree? generateTree(Map<String, int> characters) {
+  final List<NodeTree> queue = <NodeTree>[];
+
+  for (final MapEntry<String, int> item in characters.entries) {
+    queue.add(NodeTree(value: item.value, char: item.key));
+  }
+
+  while (queue.length > 1) {
+    final NodeTree left = queue.removeAt(getLowestFrequency(queue));
+    final NodeTree right = queue.removeAt(getLowestFrequency(queue));
+
+    final NodeTree newNode = NodeTree(
+      value: left.value + right.value,
+      char: '${left.char}${right.char}',
+      left: left,
+      right: right,
+    );
+
+    queue.add(newNode);
+  }
+
+  return queue.first;
+}
+
+int getLowestFrequency(List<NodeTree> queue) {
+  int index = 0;
+
+  for (int i = 0; i < queue.length; i++) {
+    if (queue[i].value < queue[index].value) {
+      index = i;
+    }
+  }
+
+  return index;
+}
+
+class NodeTree {
+  NodeTree({required this.value, this.char, this.left, this.right});
+
+  final int value;
+  final String? char;
+  final NodeTree? left;
+  final NodeTree? right;
 }
--- test/dart_huffman_coding_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:dart_huffman_coding/dart_huffman_coding.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
