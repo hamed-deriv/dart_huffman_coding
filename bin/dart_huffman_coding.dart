1310c3436f91b6744cb5575effc488a7896e61b0
hamed-fs <hamed@firstsource.io>
2021-08-03 07:02:54 +0430

    initial commit

---
+++ bin/dart_huffman_coding.dart
@@ -0,0 +1,5 @@
+import 'package:dart_huffman_coding/dart_huffman_coding.dart' as dart_huffman_coding;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_huffman_coding.calculate()}!');
+}
---
+++ test/dart_huffman_coding_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_huffman_coding/dart_huffman_coding.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
