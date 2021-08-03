4015039350368a2b42bcaa4ec55a6865ab297778
hamed-fs <hamed@firstsource.io>
2021-08-03 07:52:53 +0430

    add some helper methods

--- bin/dart_huffman_coding.dart
+++ bin/dart_huffman_coding.dart
@@ -1,5 +1,32 @@
-import 'package:dart_huffman_coding/dart_huffman_coding.dart' as dart_huffman_coding;
-
 void main(List<String> arguments) {
-  print('Hello world: ${dart_huffman_coding.calculate()}!');
+  final Map<String, int> result =
+      sortCharacters(getCharacterCount('Some Text To test'));
+
+  print(result);
+}
+
+Map<String, int> getCharacterCount(String input) {
+  final Map<String, int> result = <String, int>{};
+
+  for (int i = 0; i < input.length; i++) {
+    result.containsKey(input[i])
+        ? result[input[i]] = result[input[i]]! + 1
+        : result[input[i]] = 1;
+  }
+
+  return result;
+}
+
+Map<String, int> sortCharacters(Map<String, int> characters) {
+  final List<MapEntry<String, int>> mapEntries = characters.entries.toList()
+    ..sort(
+      (MapEntry<String, int> first, MapEntry<String, int> second) =>
+          first.value.compareTo(second.value),
+    );
+
+  characters
+    ..clear()
+    ..addEntries(mapEntries);
+
+  return characters;
 }
